070a401bd90069534ebad82819204e4cdff0443d
haeracusus <118097924+haeracusus@users.noreply.github.com>
2024-06-01 12:10:18 +0800

    splash text testing

---
+++ splashtxt.lua
@@ -0,0 +1,10 @@
+local messages = {
+'Really awesome cool and good...',
+'Blame John',
+'As sseen on TV!',
+'Awesome!',
+'100% pure!',
+'1337',
+}
+
+--get('splashtxt').textContent = messages[math.random(1,#messages)]
